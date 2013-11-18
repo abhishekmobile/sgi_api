10b88c709bd4a51a46db6c5a075ddfb252cef488
abhishekaccrete <abhishek@accretesol.com>
2013-11-18 12:50:27 +0530

    initial commit

---
+++ app/views/words/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @word, :name, :definition, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dictionary::Application.config.secret_key_base = '651a46016da934a6745d5159b29597ae94ff7d9c4d2597138a7eaad8d49214cf853c4d1b43488367389ef40ba5939cf56acabc763e2fed78a279c981e2344746'
